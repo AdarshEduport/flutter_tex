9d862af1cfc7e40351e08509499acf4ce7c11a2c
Adarsh P S <adarshps@Adarshs-MacBook-Pro.local>
2026-02-05 11:03:41 +0530

    added oveflow property

--- lib/src/styles/style.dart
+++ lib/src/styles/style.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_tex/flutter_tex.dart';
+import 'package:flutter_tex/src/styles/overflow.dart' show TeXViewOverflow, TeXViewOverflowHelper;
 import 'package:flutter_tex/src/utils/style_utils.dart';
 
 /// TeXViewStyle provide an easy interface for styling TeXView which converts dart code to CSS code.
@@ -24,6 +25,9 @@ class TeXViewStyle {
 
   ///Elevation of View.
   final int? elevation;
+
+  ///Overflow of content in TeXView.
+  final TeXViewOverflow? overflow;
 
   ///Color of content(fonts) within the View.
   final Color? contentColor;
@@ -54,6 +58,7 @@ class TeXViewStyle {
       this.backgroundColor,
       this.border,
       this.borderRadius,
+      this.overflow,
       this.textAlign,
       this.fontStyle})
       : cascadingStyleSheets = null;
@@ -69,12 +74,13 @@ class TeXViewStyle {
         contentColor = null,
         backgroundColor = null,
         border = null,
+        overflow = null,
         borderRadius = null,
         fontStyle = null,
         textAlign = null;
 
-  String? initStyle() {
+String? initStyle() {
     return cascadingStyleSheets ??
-        """$teXViewDefaultStyle ${padding?.getPadding() ?? ""}${margin?.getMargin() ?? ""}${borderRadius?.getRadius() ?? ""}${border?.getBorder() ?? ""}${height != null ? "height: ${getSizeWithUnit(height, sizeUnit)};" : ""}${width != null ? "width: ${getSizeWithUnit(width, sizeUnit)};" : ""}${elevation != null ? "box-shadow: ${getElevation(elevation, sizeUnit)};" : ""}${contentColor != null ? "color: ${getColor(contentColor)};" : ""}${backgroundColor != null ? "background-color: ${getColor(backgroundColor)};" : ""}${textAlign != null ? "text-align: ${TeXViewTextAlignHelper.getValue(textAlign)};" : ""}${fontStyle?.initFontStyle() ?? ""}""";
+        """$teXViewDefaultStyle ${padding?.getPadding() ?? ""}${margin?.getMargin() ?? ""}${borderRadius?.getRadius() ?? ""}${border?.getBorder() ?? ""}${height != null ? "height: ${getSizeWithUnit(height, sizeUnit)};" : ""}${width != null ? "width: ${getSizeWithUnit(width, sizeUnit)};" : ""}${elevation != null ? "box-shadow: ${getElevation(elevation, sizeUnit)};" : ""}${contentColor != null ? "color: ${getColor(contentColor)};" : ""}${backgroundColor != null ? "background-color: ${getColor(backgroundColor)};" : ""}${textAlign != null ? "text-align: ${TeXViewTextAlignHelper.getValue(textAlign)};" : ""}${fontStyle?.initFontStyle() ?? ""}${overflow != null ? "overflow: ${TeXViewOverflowHelper.getValue(overflow)};" : ""}""";
   }
 }
